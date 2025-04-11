e81aa21546661665934f5d42457ad46dafc34079
Vansh Gupta <83229615+g-vansh@users.noreply.github.com>
2025-04-11 14:13:46 -0400

    Updated markers

--- assets/js/research-map.js
+++ assets/js/research-map.js
@@ -44,26 +44,29 @@ document.addEventListener('DOMContentLoaded', function() {
 
   // Custom marker icons for different types of locations
   const markerIcons = {
-    current: L.divIcon({
-      className: 'custom-marker current-marker',
-      html: '<div class="marker-pin"></div>',
-      iconSize: [30, 30],
-      iconAnchor: [15, 15],
-      popupAnchor: [0, -30]
+    current: L.icon({
+      iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-blue.png',
+      shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+      iconSize: [25, 41],
+      iconAnchor: [12, 41],
+      popupAnchor: [1, -34],
+      shadowSize: [41, 41]
     }),
-    project: L.divIcon({
-      className: 'custom-marker project-marker',
-      html: '<div class="marker-pin"></div>',
-      iconSize: [30, 30],
-      iconAnchor: [15, 15],
-      popupAnchor: [0, -30]
+    project: L.icon({
+      iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png',
+      shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+      iconSize: [25, 41],
+      iconAnchor: [12, 41],
+      popupAnchor: [1, -34],
+      shadowSize: [41, 41]
     }),
-    education: L.divIcon({
-      className: 'custom-marker education-marker',
-      html: '<div class="marker-pin"></div>',
-      iconSize: [30, 30],
-      iconAnchor: [15, 15],
-      popupAnchor: [0, -30]
+    education: L.icon({
+      iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png',
+      shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+      iconSize: [25, 41],
+      iconAnchor: [12, 41],
+      popupAnchor: [1, -34],
+      shadowSize: [41, 41]
     })
   };
 
